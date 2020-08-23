7d07f5eb18448bfe412fd25c48483e6a8506cfad
Karno <karnoroid@gmail.com>
2020-08-23 15:09:29 +0900

    update impl of games.

--- src/game.py
+++ src/game.py
@@ -1,14 +1,23 @@
 """Battle Line Game System"""
 
 import copy
+import itertools
 import random
-from typing import Generic, Iterable, List, Optional, TypeVar
+from src.cardtypes import (
+    TacticEnvironments,
+    TacticMorales,
+    Tactics,
+    TroopColors,
+    Troops,
+)
+from typing import Collection, Generic, Iterable, List, Optional, TypeVar, Union
 
 from src.cards import (
     CardGenerator,
     TacticCard,
     TacticEnvironmentCard,
     TacticGuileCard,
+    TacticMoraleCard,
     TroopAndTacticMoraleCard,
     TroopCard,
 )
@@ -48,25 +57,72 @@ class Flag:
         return "\n".join(lines)
 
     def __init__(self) -> None:
-        self.side_A: List[TroopAndTacticMoraleCard] = []
-        self.side_B: List[TroopAndTacticMoraleCard] = []
-        self.side_A_env: List[TacticEnvironmentCard] = []
-        self.side_B_env: List[TacticEnvironmentCard] = []
+        self.stacks: List[List[TroopAndTacticMoraleCard]] = [[], []]
+        self.envs: List[List[TacticEnvironmentCard]] = [[], []]
+        self._last_stacked_player = PLAYER_UNRESOLVED
         self._flag_position = PLAYER_UNRESOLVED
 
-    def get_stacked_cards(self, player: int) -> Iterable[TroopAndTacticMoraleCard]:
-        if player == PLAYER_A:
-            return self.side_A
-        if player == PLAYER_B:
-            return self.side_B
-        raise ValueError(player)
+    def get_stacked_cards(self, player: int) -> Collection[TroopAndTacticMoraleCard]:
+        return self.stacks[player]
 
-    def get_stacked_envs(self, player: int) -> Iterable[TacticEnvironmentCard]:
-        if player == PLAYER_A:
-            return self.side_A_env
-        if player == PLAYER_B:
-            return self.side_B_env
-        raise ValueError(player)
+    def get_last_stacked_player(self) -> int:
+        return self._last_stacked_player
+
+    def add_stack(self, player: int, card: TroopAndTacticMoraleCard) -> None:
+        self.stacks[player].append(card)
+        self.stacks[player].sort()
+        self._last_stacked_player = player
+
+    def remove_stack_troops(
+        self, player: int, color: Union[int, TroopColors], number: Union[int, Troops]
+    ) -> Optional[TroopCard]:
+        removal: Optional[TroopCard] = None
+        for c in self.stacks[player]:
+            if (
+                isinstance(c, TroopCard)
+                and c.get_color() == color
+                and c.get_troop() == number
+            ):
+                removal = c
+                break
+        else:
+            return None
+        self.stacks[player].remove(removal)
+        return removal
+
+    def remove_stack_tacticmorales(
+        self, player: int, tactic: Union[int, TacticMorales, Tactics]
+    ) -> Optional[TacticMoraleCard]:
+        removal: Optional[TacticMoraleCard] = None
+        for c in self.stacks[player]:
+            if isinstance(c, TacticMoraleCard) and c.get_tactics() == tactic:
+                removal = c
+                break
+        else:
+            return None
+        self.stacks[player].remove(removal)
+        return removal
+
+    def add_env(self, player: int, card: TacticEnvironmentCard) -> None:
+        self.envs[player].append(card)
+        self.envs[player].sort()
+
+    # Note: Remove the environment tactics is not allowed
+    # def remove_env(
+    #     self, player: int, env: Union[int, TacticEnvironments, Tactics]
+    # ) -> Optional[TacticEnvironmentCard]:
+    #    removal: Optional[TacticEnvironmentCard] = None
+    #    for c in self.envs[player]:
+    #        if c.get_tactics() == env:
+    #            removal = c
+    #            break
+    #    else:
+    #        return None
+    #    self.envs[player].remove(removal)
+    #    return removal
+
+    def get_stacked_envs(self, player: int) -> Collection[TacticEnvironmentCard]:
+        return self.envs[player]
 
     def is_resolved(self) -> bool:
         return self._flag_position != PLAYER_UNRESOLVED
@@ -78,24 +134,33 @@ class Flag:
         assert self._flag_position == PLAYER_UNRESOLVED, "the flag is already resolved!"
         self._flag_position = player
 
+    def required_card_num(self) -> int:
+        stacked_envs = [
+            e.get_tactic_envs() for e in itertools.chain.from_iterable(self.envs)
+        ]
+        return 4 if TacticEnvironments.MUD in stacked_envs else 3
+
     def __repr__(self) -> str:
-        side_a_cards: List[Optional[TroopAndTacticMoraleCard]] = [None] * 3
-        side_b_cards: List[Optional[TroopAndTacticMoraleCard]] = [None] * 3
-        side_a_envs: List[Optional[TacticEnvironmentCard]] = [None] * 2
-        side_b_envs: List[Optional[TacticEnvironmentCard]] = [None] * 2
+        side_a_cards = [""] * 3
+        side_b_cards = [""] * 3
+        side_a_envs = [""] * 2
+        side_b_envs = [""] * 2
         flags = [" "] * 3
         for i, card in enumerate(self.get_stacked_cards(PLAYER_A)):
-            side_a_cards[i] = card
+            side_a_cards[i] = repr(card)
         for i, card in enumerate(self.get_stacked_cards(PLAYER_B)):
-            side_b_cards[i] = card
+            side_b_cards[i] = repr(card)
         for i, env in enumerate(self.get_stacked_envs(PLAYER_A)):
-            side_a_envs[i] = env
+            side_a_envs[i] = repr(env)
         for i, env in enumerate(self.get_stacked_envs(PLAYER_B)):
-            side_b_envs[i] = env
+            side_b_envs[i] = repr(env)
         flags[self.get_resolved() + 1] = "o"
         return _FLAG_REPRESENTATION_FORMAT.format(
             *side_a_cards, *side_a_envs, *side_b_cards, *side_b_envs, *flags
         )
+
+    def __eq__(self, o: object) -> bool:
+        return isinstance(o, Flag) and self.stacks == o.stacks
 
 
 TDeckCard = TypeVar("TDeckCard")
@@ -212,7 +277,7 @@ class GameState:
     def get_tactics_deck(self) -> TacticsDeck:
         return self._tactics_deck
 
-    def get_flags(self) -> Iterable[Flag]:
+    def get_flags(self) -> Collection[Flag]:
         return self._flags
 
     def get_operations(self, player: int) -> List[GuileOperation]:
--- tests/test_game.py
+++ tests/test_game.py
@@ -1,9 +1,49 @@
+# noqa
+
+from src.cardtypes import TacticEnvironments, TacticMorales, Tactics, TroopColors
+from src.cards import CardGenerator
+from src.game import Flag, PLAYER_A, PLAYER_B
 from src import game
 
 
-def test_troops_deck_len():
+def test_flag_stack_troop():  # noqa
+    flag = Flag()
+    c_r3 = CardGenerator.troop(TroopColors.RED, 3)
+    flag.add_stack(PLAYER_A, c_r3)
+    assert len(flag.get_stacked_cards(PLAYER_A)) == 1
+    assert flag.remove_stack_troops(PLAYER_A, TroopColors.RED, 3) == c_r3
+
+
+def test_flag_stack_tacticmorales():  # noqa
+    flag = Flag()
+    c_ld = CardGenerator.tactic(Tactics.LEADER_DARIUS)
+    flag.add_stack(PLAYER_B, c_ld)
+    assert len(flag.get_stacked_cards(PLAYER_B)) == 1
+    assert (
+        flag.remove_stack_tacticmorales(PLAYER_B, TacticMorales.LEADER_DARIUS) == c_ld
+    )
+
+
+def test_flag_env():  # noqa
+    flag = Flag()
+    c_mud = CardGenerator.tactic(Tactics.MUD)
+    flag.add_env(PLAYER_A, c_mud)
+    assert len(flag.get_stacked_envs(PLAYER_A)) == 1
+    assert flag.remove_env(PLAYER_A, TacticEnvironments.MUD) == c_mud
+
+
+def test_troops_deck_len():  # noqa
     assert len(game.TroopsDeck.new()) == 60
 
 
-def test_tactics_deck_len():
+def test_tactics_deck_len():  # noqa
     assert len(game.TacticsDeck.new()) == 10
+
+
+def test_flag_deepcopy():  # noqa
+    flag = Flag()
+    c_r3 = CardGenerator.troop(TroopColors.RED, 3)
+    flag.add_stack(PLAYER_A, c_r3)
+    c_ld = CardGenerator.tactic(Tactics.LEADER_DARIUS)
+    flag.add_stack(PLAYER_B, c_ld)
+
